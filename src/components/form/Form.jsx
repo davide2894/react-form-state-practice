0f78830a0d0ca981009e846202436a42e37905f8
Davide Iaiunese <davide.iaiunese.external@ynap.com>
2022-06-12 01:23:42 +0200

    add state management logic

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -18,40 +18,79 @@ export default function Form(){
      *    up for our newsletter!" to the console.
      */
     
-     function handleSubmit(event) {
+    const [formData, setFormData] = useState({
+        email: "",
+        password: "",
+        confirmPassword: "",
+        marketingCheckbox: ""
+    })
+
+    function handleSubmit(event) {
         event.preventDefault()
+    }
+
+    function handleChange(evt) {
+        const {name, value, type, checked} = evt.target;
+        setFormData(prevFormData => {
+            return {
+                ...prevFormData,
+                [name]: type === "checkbox" ? checked : value
+            }
+        })
+    }
+
+    function handleSubmit(evt) {
+        evt.preventDefault();
+        if(formData.password === formData.confirmPassword) {
+            console.log("Successfully signed up");
+        } else {
+            console.log("passwords to not match");
+        }
+
+        if(formData.marketingCheckbox   ) {
+            console.log("Thanks for signing up for our newsletter!");
+        }
     }
     
     return (
         <div className="form-container">
-            <form className="form" onSubmit={handleSubmit}>
+            <form onSubmit={handleSubmit} className="form" onSubmit={handleSubmit}>
                 <input 
                     type="email" 
                     placeholder="Email address"
                     className="form--input"
+                    name="email"
+                    onChange={handleChange}
+                    value={formData.email}
                 />
                 <input 
                     type="password" 
                     placeholder="Password"
                     className="form--input"
+                    name="password"
+                    onChange={handleChange}
+                    value={formData.password}
                 />
                 <input 
                     type="password" 
                     placeholder="Confirm password"
                     className="form--input"
+                    name="confirmPassword"
+                    onChange={handleChange}
+                    value={formData.confirmPassword}
                 />
                 
                 <div className="form--marketing">
                     <input
                         id="okayToEmail"
                         type="checkbox"
-                        
+                        name="marketingCheckbox"
+                        onChange={handleChange}
+                        checked={formData.marketingCheckbox}
                     />
                     <label htmlFor="okayToEmail">I want to join the newsletter</label>
                 </div>
-                <button 
-                    className="form--submit"
-                >
+                <button className="form--submit">
                     Sign up
                 </button>
             </form>
